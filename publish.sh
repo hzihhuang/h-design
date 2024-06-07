972dc3807571f0d9abeceb93648b9c55978828a0
v_hzihhuang <61496900+Super-ZiHao@users.noreply.github.com>
2024-06-07 10:45:34 +0800

    docs: 发布-同步本地

--- publish.sh
+++ publish.sh
@@ -1,20 +1,15 @@
-# 将 gh-pages 分支同步到 gitee 仓库 master 上
-
 #!/bin/bash
-
 # 添加 Gitee 远程仓库
 if git remote | grep -q gitee; then
     echo "远程仓库已存在，执行下一步..."
 else
-    # gitee 仓库
     git remote add gitee https://gitee.com/hzihhuang/h-design.git
-    # github 仓库
     git remote add github https://github.com/hzihhuang-site/h-design.git
-    echo "成功添加 Gitee 远程仓库，执行下一步..."
+    echo "成功添加 Gitee 远程仓库..."
+    exit 1
 fi
 
 # 推送 gh-pages 分支到 gitee 的 master 分支
-
 git pull
 if [ $? -eq 0 ]; then
     echo "获取最新代码成功，执行下一步..."
