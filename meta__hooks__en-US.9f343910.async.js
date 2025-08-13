fd9faed560779eb780867770c592c42873744ac5
hzihhuang <3306084512@qq.com>
2025-08-13 09:35:39 +0000

    Updates

--- meta__hooks__en-US.9f343910.async.js
+++ meta__hooks__en-US.9f343910.async.js
@@ -49,15 +49,17 @@ import React from 'react';
 const Index = () => {
   const [{ key1, key2, key3 }, dispatchStorage] = useLocalStorageState(['key1', 'key2', 'key3'] as const);
   return (
-    <div className="">
-      <div className="flex items-center gap-4">
+    <div className="flex flex-col gap-12">
+      <div>
         <div>{key1}</div>
-        <div>{key2}</div>
-        <div>{key3}</div>
+        <input className="border" value={key1} type="text" onChange={(e) => dispatchStorage('key1', e.target.value)} />
       </div>
       <div>
-        <input className="border" value={key1} type="text" onChange={(e) => dispatchStorage('key1', e.target.value)} />
+        <div>{key2}</div>
         <input className="border" value={key2} type="text" onChange={(e) => dispatchStorage('key2', e.target.value)} />
+      </div>
+      <div>
+        <div>{key3}</div>
         <input className="border" value={key3} type="text" onChange={(e) => dispatchStorage('key3', e.target.value)} />
       </div>
     </div>
